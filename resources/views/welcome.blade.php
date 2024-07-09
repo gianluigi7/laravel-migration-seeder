4e236ff4f2eb96c43f9ebb6ba0fccb4b2eeb9bc0
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-07-09 22:03:06 +0200

    fine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,23 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    <h1>template Laravel</h1>
-    <h2>prova</h2>
+    <h1>Trains</h1>
+    <ul>
+        @foreach ($trains as $train)
+        <li>
+          <p>{{ $train->azienda }}</p>
+          <p>direzione {{ $train->stazione_di_partenza }}</p>
+          <p> diretto a {{ $train->stazione_di_arrivo }}</p>
+          <p>numero di carrozze: {{ $train->numero_carrozze }}</p>
+          <p>{{ $train->codice_treno }}</p>
+          <p>partenza {{ \Illuminate\Support\Carbon::parse($train['orario_di_partenza'])->format('d/m/Y')}}</p>
+          <p>arrivo {{ \Illuminate\Support\Carbon::parse($train['orario_di_arrivo'])->format('d/m/Y')}}</p>
+         @if($train->in_orario) <p>in orario </p> @endif
+         @if($train->cancellato) <p> cancellato</p> @endif
+          
+        </li>
+        @endforeach
+    </ul>
 
 <!-- ESEMPIO IMG
 <img src="{{ Vite::asset('resources/img/logo.png') }}" alt=""> -->
